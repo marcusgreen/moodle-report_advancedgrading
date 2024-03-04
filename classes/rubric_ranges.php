0430491721d51e2acebb04374b421cb9b6ca73ab
Guillaume Barat <guillaumebarat@catalyst-au.net>
2024-03-04 11:42:51 +1000

    fixed issue #16 / add rubric range support

---
+++ classes/rubric_ranges.php
@@ -0,0 +1,105 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This is the external API for this report.
+ *
+ * @package    report_advancedgrading
+ * @copyright  2024 Guillaume Barat
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace report_advancedgrading;
+
+/**
+ * Logic to process data for assignments using the rubric range grading method
+ *
+ * @copyright  2024 Guillaume Barat
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class rubric_ranges {
+
+    /**
+     * Assemble the table rows for grading information in an array from the database records returned.
+     * for each student
+     *
+     * @param array $data
+     * @return string
+     */
+    public function get_rows(array $data): string {
+        $rows = '';
+        if (isset($data['students'])) {
+            foreach ($data['students'] as $student) {
+                $rows .= '<tr>';
+                $rows .= get_student_cells($data, $student);
+                foreach (array_keys($data['criterion']) as $crikey) {
+                    $rows .= '<td>' . $student['grades'][$crikey]['score'] . '</td>';
+                    $rows .= '<td>' . $student['grades'][$crikey]['definition'] . '</td>';
+                    $rows .= '<td>' . $student['grades'][$crikey]['feedback'] . '</td>';
+                }
+                $rows .= get_summary_cells($student);
+                $rows .= '</tr>';
+            }
+        }
+        if ($rows == "") {
+            $rows .= '<tr> <td>' . get_string('nomarkedsubmissions', 'report_advancedgrading') . '</td>';
+            for ($i = 0; $i < $data['colcount'] - 1; $i++) {
+                $rows .= '<td></td>';
+            }
+            $rows .= '</tr>';
+        }
+        return $rows;
+    }
+
+    /**
+     * Query the database for the student grades.
+     *
+     * @param \assign $assign
+     * @param \cm_info $cm
+     * @return array
+     */
+    public function get_data(\assign $assign, \cm_info $cm): array {
+        global $DB;
+        $sql = "SELECT grf.id as grfid,
+                        cm.course, asg.name as assignment,asg.grade as gradeoutof,
+                        criteria.description, level.score,
+                        level.definition, grf.remark, grf.criterionid,
+                        stu.id AS userid, stu.idnumber AS idnumber,
+                        stu.firstname, stu.lastname, stu.username,
+                        stu.username, stu.email, rubm.username AS grader,
+                        ag.timemodified AS modified,
+                        ctx.instanceid, ag.grade, asg.blindmarking, assign_comment.commenttext as overallfeedback
+                    FROM {assign} asg
+                    JOIN {course_modules} cm ON cm.instance = asg.id
+                    JOIN {context} ctx ON ctx.instanceid = cm.id
+                    JOIN {grading_areas}  ga ON ctx.id=ga.contextid
+                    JOIN {grading_definitions} gd ON ga.id = gd.areaid
+                    JOIN {gradingform_rubric_ranges_c} criteria ON (criteria.definitionid = gd.id)
+                    JOIN {gradingform_rubric_ranges_l} level ON (level.criterionid = criteria.id)
+                    JOIN {grading_instances} gin ON gin.definitionid = gd.id
+                    JOIN {assign_grades} ag ON ag.id = gin.itemid
+            LEFT  JOIN {assignfeedback_comments} assign_comment on assign_comment.grade = ag.id
+                    JOIN {user} stu ON stu.id = ag.userid
+                    JOIN {user} rubm ON rubm.id = ag.grader
+                    JOIN {gradingform_rubric_ranges_f} grf ON (grf.instanceid = gin.id)
+                    AND (grf.criterionid = criteria.id) AND (grf.levelid = level.id)
+                WHERE cm.id = :assignid 
+                ORDER BY lastname ASC, firstname ASC, userid ASC, criteria.sortorder ASC";
+        $data = $DB->get_records_sql($sql, ['assignid' => $cm->id]);
+        $data = set_blindmarking($data, $assign, $cm);
+        return $data;
+    }
+}
